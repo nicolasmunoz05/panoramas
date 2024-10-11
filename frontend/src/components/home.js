9091556c076d68580f0fa7a4397ac3633dfdba70
Nicolas <nicolasmunoztoloza@gmail.com>
2024-10-11 03:46:33 -0300

    Rehechos y ordenados componentes de home, falta mucho

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -6,19 +6,12 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const Home = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  // Función para alternar el menú hamburguesa
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
   // Configuración del carrusel de noticias
   const settings = {
     dots: false,
     infinite: true,
     speed: 500,
-    slidesToShow: 6,
+    slidesToShow: 5,
     slidesToScroll: 1,
     responsive: [
       {
@@ -42,135 +35,220 @@ const Home = () => {
     <Container fluid>
       <Row>
         {/* Navbar */}
-        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <nav className="navbar navbar-expand-lg navbar-light">
           <div className="container-fluid">
-            <a className="navbar-brand" href="#">☰</a>
-            <a className="navbar-brand-center" href="#">ChilExplora!</a>
+            <a className="navbar-brand" href="#">
+              ☰
+            </a>
+            <a className="navbar-brand-center" href="#">
+              ChilExplora!
+            </a>
             {/* Links Navbar */}
-              <ul className="navbar-nav ms-auto">
-                <li className="nav-item">
-                  <a className="nav-link" href="#">Publica con Nosotros</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">Iniciar Sesión</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">Región</a>
-                </li>
-              </ul>
-            </div>
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Publica con Nosotros
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Iniciar Sesión
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Región
+                </a>
+              </li>
+            </ul>
+          </div>
         </nav>
       </Row>
 
+      {/* Header */}
+      <div className="header-container">
+        <h2>Bienvenido, Invitado</h2>
+
+        <button type="button" className="btn-map">
+          Mapa 📍
+        </button>
+
+        <div className="search-bar">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Buscar eventos, panoramas..."
+          />
+        </div>
+      </div>
+
 
       {/* Banner Carousel */}
-      <div className="carousel-container">
-        <h2>Bienvenido, Invitado</h2>
+      <div className="banner-carousel">
         <Carousel>
           <Carousel.Item>
-            <img className="d-block w-100" src="https://via.placeholder.com/1200x400" alt="First slide" />
+            <img
+              className="d-block w-100"
+              src="https://via.placeholder.com/1200x400"
+              alt="First slide"
+            />
             <Carousel.Caption>
               <h3>Evento 1</h3>
               <p>Descripción del evento 1.</p>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
-            <img className="d-block w-100" src="https://via.placeholder.com/1200x400" alt="Second slide" />
+            <img
+              className="d-block w-100"
+              src="https://via.placeholder.com/1200x400"
+              alt="Second slide"
+            />
             <Carousel.Caption>
               <h3>Evento 2</h3>
               <p>Descripción del evento 2.</p>
             </Carousel.Caption>
           </Carousel.Item>
         </Carousel>
-        
-      {/* Barra Busqueda */}
-        <Row className="mt-2">
-          <div className="search-bar">
-            <input type="text" className="form-control" placeholder="Buscar eventos, panoramas..." />  
-          </div>
-
-    <button type="button" className="btn btn-info mt-2">
-      Mapa
-    </button>
-        </Row>
       </div>
 
       {/* Body - Eventos */}
-      <Row className="mt-2">
-        <div className="carousel-container mt-1">
+      <Row>
+        <div className="carousel-container">
           <h2>Eventos de esta semana</h2>
           <Slider {...settings}>
             <div className="news-item">
-              <img src="https://via.placeholder.com/300x300" alt="Noticia 1" className="news-image" />
-              <p className="news-text">Evento 1: Descripción breve del Evento</p>
+              <img
+                src="https://via.placeholder.com/300x300"
+                alt="Noticia 1"
+                className="news-image"
+              />
+              <p className="news-text">
+                Evento 1: Descripción breve del Evento
+              </p>
             </div>
             <div className="news-item">
-              <img src="https://via.placeholder.com/300x300" alt="Evento 2" className="news-image" />
-              <p className="news-text">Evento 2: Descripción breve del Evento</p>
+              <img
+                src="https://via.placeholder.com/300x300"
+                alt="Evento 2"
+                className="news-image"
+              />
+              <p className="news-text">
+                Evento 2: Descripción breve del Evento
+              </p>
             </div>
             <div className="news-item">
-              <img src="https://via.placeholder.com/300x300" alt="Evento 3" className="news-image" />
-              <p className="news-text">Evento 3: Descripción breve del Evento</p>
+              <img
+                src="https://via.placeholder.com/300x300"
+                alt="Evento 3"
+                className="news-image"
+              />
+              <p className="news-text">
+                Evento 3: Descripción breve del Evento
+              </p>
             </div>
             <div className="news-item">
-              <img src="https://via.placeholder.com/300x300" alt="Evento 4" className="news-image" />
-              <p className="news-text">Evento 4: Descripción breve del Evento</p>
+              <img
+                src="https://via.placeholder.com/300x300"
+                alt="Evento 4"
+                className="news-image"
+              />
+              <p className="news-text">
+                Evento 4: Descripción breve del Evento
+              </p>
             </div>
           </Slider>
         </div>
       </Row>
 
       {/* Body - Noticias */}
-      <Row className="mt-2">
-        <div className="carousel-container mt-1">
+      <Row className="">
+        <div className="carousel-container">
           <h2>Noticias Recientes</h2>
           <Slider {...settings}>
             <div className="news-item">
-              <img src="https://via.placeholder.com/300x300" alt="Noticia 1" className="news-image" />
-              <p className="news-text">Noticia 1: Descripción breve de la noticia</p>
+              <img
+                src="https://via.placeholder.com/300x300"
+                alt="Noticia 1"
+                className="news-image"
+              />
+              <p className="news-text">
+                Noticia 1: Descripción breve de la noticia
+              </p>
             </div>
             <div className="news-item">
-              <img src="https://via.placeholder.com/300x300" alt="Noticia 2" className="news-image" />
-              <p className="news-text">Noticia 2: Descripción breve de la noticia</p>
+              <img
+                src="https://via.placeholder.com/300x300"
+                alt="Noticia 2"
+                className="news-image"
+              />
+              <p className="news-text">
+                Noticia 2: Descripción breve de la noticia
+              </p>
             </div>
             <div className="news-item">
-              <img src="https://via.placeholder.com/300x300" alt="Noticia 3" className="news-image" />
-              <p className="news-text">Noticia 3: Descripción breve de la noticia</p>
+              <img
+                src="https://via.placeholder.com/300x300"
+                alt="Noticia 3"
+                className="news-image"
+              />
+              <p className="news-text">
+                Noticia 3: Descripción breve de la noticia
+              </p>
             </div>
             <div className="news-item">
-              <img src="https://via.placeholder.com/300x300" alt="Noticia 4" className="news-image" />
-              <p className="news-text">Noticia 4: Descripción breve de la noticia</p>
+              <img
+                src="https://via.placeholder.com/300x300"
+                alt="Noticia 4"
+                className="news-image"
+              />
+              <p className="news-text">
+                Noticia 4: Descripción breve de la noticia
+              </p>
             </div>
           </Slider>
         </div>
       </Row>
 
       {/* Body - Noticias */}
-      <Row className="mt-2">
-        <div className="carousel-container mt-1">
+      <Row>
+        <div className="carousel-container">
           <h2>Cultura</h2>
           <Slider {...settings}>
             <div className="news-item">
-              <img src="https://via.placeholder.com/300x300" alt="Noticia 1" className="news-image" />
+              <img
+                src="https://via.placeholder.com/300x300"
+                alt="Noticia 1"
+                className="news-image"
+              />
               <p className="news-text">Evento Cultural 1: Descripción breve</p>
             </div>
             <div className="news-item">
-              <img src="https://via.placeholder.com/300x300" alt="Noticia 2" className="news-image" />
+              <img
+                src="https://via.placeholder.com/300x300"
+                alt="Noticia 2"
+                className="news-image"
+              />
               <p className="news-text">Evento Cultural 2: Descripción breve</p>
             </div>
             <div className="news-item">
-              <img src="https://via.placeholder.com/300x300" alt="Noticia 3" className="news-image" />
+              <img
+                src="https://via.placeholder.com/300x300"
+                alt="Noticia 3"
+                className="news-image"
+              />
               <p className="news-text">Evento Cultural 3: Descripción breve</p>
             </div>
             <div className="news-item">
-              <img src="https://via.placeholder.com/300x300" alt="Noticia 4" className="news-image" />
+              <img
+                src="https://via.placeholder.com/300x300"
+                alt="Noticia 4"
+                className="news-image"
+              />
               <p className="news-text">Evento Cultural 4: Descripción breve</p>
             </div>
           </Slider>
         </div>
       </Row>
-
-
     </Container>
   );
 };
